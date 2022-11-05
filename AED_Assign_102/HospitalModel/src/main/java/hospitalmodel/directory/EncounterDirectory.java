28fc86fc12205a05f40cc5966edd8ff0ae3df7c6
Akshathapatil1998 <71171604+Akshathapatil1998@users.noreply.github.com>
2022-11-05 14:45:38 -0400

    Revert "Revert "Validations""
    This reverts commit 542e411c23408bb155a0639ed09f8548817fc54c.

---
+++ AED_Assign_102/HospitalModel/src/main/java/hospitalmodel/directory/EncounterDirectory.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmodel.directory;
+
+import hospitalmodel.models.Encounter;
+import hospitalmodel.models.VitalSigns;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author abhilashgp
+ */
+public class EncounterDirectory {
+    
+    ArrayList<Encounter> directory;
+
+    public EncounterDirectory() {
+        this.directory = new ArrayList<>();
+    }
+
+    public ArrayList<Encounter> getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(ArrayList<Encounter> directory) {
+        this.directory = directory;
+    }
+    
+    public Encounter addNewEncounter(Timestamp timestamp){
+    Encounter encounter = new Encounter(timestamp);
+    directory.add(encounter);
+    return encounter;
+    }
+    
+    public void deleteCityAccount(Encounter encounter){
+        
+        ListIterator<Encounter> var = directory.listIterator();
+
+        while (var.hasNext()) {
+
+            if (var.next().equals(encounter)){
+              var.remove();
+            }
+        }
+    }
+    
+    public Encounter getCityAccount(Encounter encounter) {
+
+        Encounter encounterAccountLocal = null;
+
+        for(Encounter ci : directory){
+            if(ci.equals(encounter))
+                encounterAccountLocal =  encounter;
+        }
+
+         return encounterAccountLocal;
+    }
+    
+    
+}
---
+++ AED_Assign_102/HospitalModel/src/main/java/hospitalmodel/models/City.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmodel.models;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author abhilashgp
+ */
+public class City {
+    
+    private String States;
+    private String Cities;
+    private Community community;
+    
+    public City(String States, String Cities, Community community) {
+        this.States = States;
+        this.Cities = Cities;
+        this.community = community;
+    }
+    
+    public String getCities() {
+        return Cities;
+    }
+
+    public void setCities(String Cities) {
+        this.Cities = Cities;
+    }
+    
+    public String getStates() {
+        return States;
+    }
+
+    public void setStates(String Name) {
+        this.States = Name;
+    }
+
+    public Community getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(Community community) {
+        this.community = community;
+    }
+   
+    @Override
+    public String toString() {
+        return "City{" + "States=" + States + ", Cities=" + Cities + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.States);
+        hash = 79 * hash + Objects.hashCode(this.Cities);
+        hash = 79 * hash + Objects.hashCode(this.community);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final City other = (City) obj;
+        if (!Objects.equals(this.States, other.States)) {
+            return false;
+        }
+        if (!Objects.equals(this.Cities, other.Cities)) {
+            return false;
+        }
+        return Objects.equals(this.community, other.community);
+    }
+    
+       
+}
